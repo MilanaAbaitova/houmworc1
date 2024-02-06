6616906c56454c0ab6ae1953ee2b3ee30660ad74
MilanaAbaitova <lanekm@mail.ru>
2024-02-06 10:18:53 +0300

    Исправление hw3

--- hw-3/index.js
+++ hw-3/index.js
@@ -1,6 +1,6 @@
 // Задание-1
 
-let password = String(prompt('Введите пароль'));
+let password = (prompt('Введите пароль'));
 let chek;
 if (password === 'Пароль') {
     chek = 'Пароль введен верно';
@@ -41,37 +41,22 @@ alert(Number(a) + Number(b));
 let monthNumber = prompt('Введите номер месяца');
 switch (monthNumber) {
     case '12':
-        console.log('Зима');
-        break;
     case '1':
-        console.log('Зима');
-        break;
     case '2':
         console.log('Зима');
         break;
     case '3':
-        console.log('Весна');
-         break;
-    case '4': console.log('Весна')
-        ; break;
+    case '4':
     case '5':
         console.log('Весна');
         break;
-    case '6':
-        console.log('Лето');
-        break;
+    case `6`:
     case '7':
-        console.log('Лето');
-        break;
     case '8':
         console.log('Лето');
         break;
     case '9':
-        console.log('Осень');
-        break;
     case '10':
-        console.log('Осень');
-        break;
     case '11':
         console.log('Осень');
         break;
