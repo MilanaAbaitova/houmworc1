c15f1d051ea645e69716eb7abf1b24e452d2d705
MilanaAbaitova <lanekm@mail.ru>
2024-02-23 18:05:10 +0300

    верстка

---
+++ №1/index.js
@@ -0,0 +1,17 @@
+function monthNum() {
+
+    let month = prompt(`Введите номер месяца`);
+
+    if ((month >= 1 && month <= 2) || month === 12) {
+        alert("Зима");
+    } else if (month >= 3 && month <= 5) {
+        alert("Весна");
+    } else if (month >= 6 && month <= 8) {
+        alert("Лето");
+    } else if (month >= 9 && month <= 11) {
+        alert("Осень");
+    } else {
+        alert("Такого месяца нет");
+    }
+
+}
